69f0e3fdb5cb5769c425691b969e6e3c5fdf2ef8
Abhishek <am2524471@gmail.com>
2026-07-28 20:02:28 +0530

    "Feat: add random Quotes, gradients, click counter and button animation"
    Added random motivational quotes with dynamic gradient backgrounds, random text colors, a click counter, and a button scale animation to improve the interactivity and user experience of the project.

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
 let heading = document.getElementById("heading");
 let btn = document.getElementById("btn");
+let countText = document.getElementById("count") 
+let count = 0;
 
-heading.style.color="brown";
 
 btn.addEventListener("click" , function(){
-let colors =["red" , "blue", "green", "yellow", "pink", "black"];
-let randomIndex = Math.floor(Math.random() * colors.length);
-document.body.style.backgroundColor = colors[randomIndex];
-heading.textContent="I'm Back."
+
+//For Count The Clicks.
+count++;
+countText.textContent = "You Clicked "+count+" Times.";
+
+//For Adding some scale animation.
+btn.classList.add("scale");
+
+setTimeout(function(){
+ btn.classList.remove("scale");
+}, 200)
+
+//For the color  gradient effect.
+let gradients =[
+"linear-gradient(135deg, #667eea, #764ba2)",
+"linear-gradient(135deg, #ff6a00, #ee0979)",
+"linear-gradient(135deg, #11998e, #38ef7d)",
+"linear-gradient(135deg, #fc466b, #3f5efb)",
+"linear-gradient(135deg, #f7971e, #ffd200)"
+];
+let randomGradient = Math.floor(Math.random()*gradients.length);
+document.body.style.background = gradients[randomGradient];
+
+
+
+//For random thoughts.
+let toughts =["Small steps every day lead to big results.🚀",
+"Dream big, start small, stay consistence.💪",
+"Sucess begins with self-belief.✨",
+"Your only limit is your mindset.🧠",
+"Make today so awesome that yesterday gets jealous.🔥"];
+let rondomToughts = Math.floor(Math.random()*toughts.length);
+heading.textContent= toughts[rondomToughts];
+
+//For Background color change on click.
+let textColors =["white", "black", "white", "black"];
+let randomIndex = Math.floor(Math.random() * textColors.length);
+heading.style.color = textColors[randomIndex];
+
+//Animation for the random text or Quote's.
 heading.classList.remove("animate");
 void heading.offsetWidth;
 heading.classList.add("animate");
+
 
 });
